f28b4b8688246ec5dbf2510f6eff389abc02e70a
misaraadel <misaraadel@gmail.com>
2021-11-05 22:24:26 +0200

    end project

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,61 +7,75 @@ $(document).ready(function () {
     //scroll to top make fade for button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 700) {
-            $(".scroll-top-button").fadeIn();
+            $(".scroll-top-btn").addClass('active');
         } else {
-            $(".scroll-top-button").fadeOut();
+            $(".scroll-top-btn").removeClass('active');
         }
     });
     //scroll to top button
-    $(".scroll-top-button").on('click', function () {
+    $(".scroll-top-btn").on('click', function () {
         $('html , body').animate({
             scrollTop: 0
         }, 100);
     });
-     //Wow intit
-     wow = new WOW({
-        boxClass: 'wow',
-        animateClass: 'animated',
-        offset: 200,
-        mobile: true,
-        live: false
+    $('.fancybox').fancybox({
+        arrows: false,
+        loop: false,
+        keyboard: false,
+        infobar: false,
+        protect: true,
+        hideScrollbar: true,
+        touch: false ,
+        image: {
+            preload: true
+        },
+        youtube : {
+            controls : 0,
+            showinfo : 0
+        },
+        vimeo : {
+            color : 'f00'
+        }
     });
-    wow.init();
     //start active navbar
-    $('.nav-button').click(function(){
-        $('.mobile-nav').addClass('active-nav');
-    });
-    $('.mobile-close').click(function(){
-        $('.mobile-nav').removeClass('active-nav');
+    $('.hamburger').click(function(){
+        $('.hamburger').toggleClass('active');
+        $('.navbar-nav').toggleClass('acitve-nav');
+        $('body').toggleClass('overflowNone')
     });
     //owl header screen 
     $('.owl-header').owlCarousel({
+        rtl: true,
         loop:true,
         margin: 20,
-        nav:false,
+        nav:true,
         dots: true,
-        autoplay: true,
+        autoplay: false,
         touchDrag  :false,
 		mouseDrag  : false,
 		autoWidth: false,
 		animateOut: 'fadeOut',
         animateIn: 'fadeIn',
-        autoplayTimeout: 2000,
 		smartSpeed: 2000,
 		dragEndSpeed: 2000,
 		slidSpeed: 900,
         paginationSpeed: 900,
         autoplayHoverPause: true,
-        items:1
+        navContainerClass: "container owl-nav",
+		navText: [
+            '<i class="fas fa-chevron-right"></i>',
+            '<i class="fas fa-chevron-left"></i>'
+        ],
+        items:1,
     });
     //partner slider 
-    $('.owl-partner').owlCarousel({
+    $('.owl-projects').owlCarousel({
         rtl: true,
-        loop:false,
+        loop:true,
         margin: 20,
-        nav:false,
-        dots: true,
-        autoplay: true,
+        nav:true,
+        dots: false,
+        autoplay: false,
         touchDrag  : true,
 		mouseDrag  : true,
 		autoWidth: false,
@@ -73,6 +87,11 @@ $(document).ready(function () {
 		slidSpeed: 900,
         paginationSpeed: 900,
         autoplayHoverPause: true,
+        navContainerClass: "container owl-nav",
+		navText: [
+            '<i class="fas fa-chevron-right"></i>',
+            '<i class="fas fa-chevron-left"></i>'
+        ],
         responsive:{
             0:{
                 items:1
@@ -81,7 +100,7 @@ $(document).ready(function () {
                 items:2
             },
             1000:{
-                items:5
+                items:3
             }
         }
     });
